88363708250372646c245912d1ef451412911ca8
Fragata <fragata8@yahoo.com>
2012-05-30 19:02:57 +0200

    haha

---
+++ project/src/main/java/rinde/sim/lab/model/virtual/VirtualEntity.java
@@ -0,0 +1,15 @@
+package rinde.sim.lab.model.virtual;
+
+import rinde.sim.core.graph.Point;
+
+public interface VirtualEntity {
+	
+	public void init(GradientFieldAPI api);
+
+	public boolean isEmitting();
+	
+	public Point getPosition();
+	
+	public FieldData getFieldData();
+	
+}
